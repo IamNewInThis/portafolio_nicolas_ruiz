d4d56968d49edcccf42d55625ce6ca16291f4ded
Nicolas Ruiz <ni.ruizz@duocuc.cl>
2025-07-07 10:31:29 -0400

    rutas

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ import {Projects} from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer"
 import ProjectDetail from "./components/ProjectDetail";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/portafolio_nicolas_ruiz">
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<><Banner /><Skills /><Projects /><Footer /></>} />
-          <Route path="/projects/:category/:slug" element={<ProjectDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<><Banner /><Skills /><Projects /><Footer /></>} />
+        <Route path="/projects/:category/:slug" element={<ProjectDetail />} />
+      </Routes>
+    </div>
   );
 }
 
